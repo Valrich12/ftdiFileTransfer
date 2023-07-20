d8aa4f282ac0272c04cbe4d1c7b272726086e5ae
Valrich12 <varica12@outlook.com>
2023-07-20 14:10:03 -0600

    VERSION 3

--- main.py
+++ main.py
@@ -31,14 +31,51 @@ def save_file(file_name, data):
         archivo.write(data)
 
 
+def format_data(data, address, command):
+    # Split the data into chunks
+    chunks = [data[i:i + 8] for i in range(0, len(data), 8)]
+    # Concatenate additional bits to each chunk
+    formatted_chunks = []
+    for chunk in chunks:
+        # Convert chunk to bytearray
+        chunk_bytearray = bytearray(chunk)
+
+        # Convert address and command to bytearray
+        address_bytearray = bytearray(8)
+        command_bytearray = bytearray(1)
+
+        for i in range(8):
+            address_bytearray[-(i + 1)] = address >> (8 * i) & 0xFF
+
+        command_bytearray[-(0 + 1)] = command >> 0 & 0xFF
+
+        # Concatenate the bytearrays
+        concatenated_chunk = chunk_bytearray + address_bytearray + command_bytearray
+
+        # Append to the list
+        formatted_chunks.append(concatenated_chunk)
+        address = address + 1
+
+    # Return the list of bytearrays
+    return formatted_chunks
+
+
 def send_file():
     file_name = label3.cget("text")
+
     data = read_file(file_name)
-    port1 = serial.Serial("COM4", 115200, timeout=1)
+    address = int(addressEntry.get())
+    command = int(commandEntry.get())
+
+    formatted_data = format_data(data, address, command)
+
+    # port1 = serial.Serial("COM4", 115200, timeout=1)
     try:
         #
-        send_data(data, port1)
-
+        testfile = open('test.hex', 'wb')
+        for data_package in formatted_data:
+            testfile.write(data_package)
+            # send_data(data_package, port1)
         # Only for tests
         # received_data = receive_data(port1, len(data))
         # save_file("prueba.rar", received_data)
@@ -47,8 +84,8 @@ def send_file():
         print("There was an error trying to send the file", e)
 
     finally:
-        #
-        port1.close()
+        print("test successful")
+        # port1.close()
 
 
 def browse_file():
@@ -72,6 +109,12 @@ label3.pack(pady=12, padx=10)
 
 chooseFileButton = customtkinter.CTkButton(master=frame, text="CHOOSE FILE", command=browse_file)
 chooseFileButton.pack(pady=12, padx=10)
+
+addressEntry = customtkinter.CTkEntry(master=frame,placeholder_text="ADDRESS")
+addressEntry.pack(pady=12, padx=10)
+
+commandEntry = customtkinter.CTkEntry(master=frame,placeholder_text="COMMAND")
+commandEntry.pack(pady=12, padx=10)
 
 sendButton = customtkinter.CTkButton(master=frame, text="SEND", command=send_file)
 sendButton.pack(pady=12, padx=10)
